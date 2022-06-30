bbdda53e305d4d26b78e40c3f095b80780ef13eb
Betty Godier <betty.godier@gmail.com>
2022-06-30 14:53:00 +0200

    Initial commit

---
+++ Prototype/Prototype/PrototypeApp.swift
@@ -0,0 +1,14 @@
+//
+//      2022  Betty.dev 
+//
+
+import SwiftUI
+
+@main
+struct PrototypeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
